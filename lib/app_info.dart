4ba2a2dd31d7be742aaacb452c63b781d255978f
DALI951 <DALI951@users.noreply.github.com>
2026-09-13 08:42:55 +0000

    v0.4.3: bump 0.4.3+8 (code 8)

--- lib/app_info.dart
+++ lib/app_info.dart
@@ -7,7 +7,7 @@ class AppInfo {
   AppInfo._();
 
   static const String name = 'MiniGames';
-  static const String version = '0.4.2';
+  static const String version = '0.4.3';
   static const String repo = 'DALI951/mini-games';
   static const String repoUrl = 'https://github.com/DALI951/mini-games';
 
